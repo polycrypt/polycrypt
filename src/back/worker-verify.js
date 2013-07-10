77c2e374f171a914869efa56a974fba4b71afd42
unknown <jimsch@Philemon.augustcellars.com>
2013-07-10 14:18:04 -0700

    When doing comparisons of MAC values, the computed MAC must be truncated to the same length as the MAC that was fed in for verification

--- src/back/worker-verify.js
+++ src/back/worker-verify.js
@@ -141,7 +141,14 @@ Impl.extend({
                         hash = libpolycrypt.hmac_sha256(this.key.key, data);
                         break;
                 }
-                ver = (hash == this.signature);
+                if (hash.length != this.signature.length) {
+                    if (hash.length < this.signature.length) {
+                        this.die('Signature has too many bytes');
+                    }
+                    var subSig = hash.subarray(0, this.signature.length);
+                    ver = (subSig == this.signature);
+                }
+                else ver = (hash == this.signature);
                 break;
 
             case 'RSASSA-PKCS1-v1_5':
